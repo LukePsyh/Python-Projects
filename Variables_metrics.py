4d976c02179314c1d90801e6403cc317a1de1d28
LukePsyh <100484107+LukePsyh@users.noreply.github.com>
2023-04-11 21:38:54 -0400

    Add files via upload

---
+++ Variables_metrics.py
@@ -0,0 +1,32 @@
+# Luke Psyhogios, 1 February 2022, Section 001, Problem #3: Math Expressions
+
+# Assign initial mm variable
+mm = 750
+
+# Compute mm conversions using math expressions
+inch = mm*0.0394
+link = inch/7.92
+ft = inch/12
+yd = ft/3
+rod = link/25
+chain = rod/4
+mile = yd/1760
+
+
+# Display top of table
+print('----------------------------------------------------------------')
+print('               mm to US length units                            ')
+print('----------------------------------------------------------------')
+# Display conversion of 750 mm to all other variables
+print('       mm:             ', mm)
+print('       in:             ', inch)
+print('       links:          ', link)
+print('       ft:             ', ft)
+print('       yds:            ', yd)
+print('       rods:           ', rod)
+print('       chains:         ', chain)
+print('       mi:             ', mile)
+# Display bottom of table
+print('----------------------------------------------------------------')
+
+      
