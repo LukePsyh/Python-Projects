9759fc0bcf83ff3f74caf4434f77fcbc5d60430f
LukePsyh <100484107+LukePsyh@users.noreply.github.com>
2023-04-11 21:38:39 -0400

    Add files via upload

---
+++ Variables_grades.py
@@ -0,0 +1,21 @@
+# Luke Psyhogios, 1 February 2022, Section 001, Problem #1: Variables & Print Statements
+
+# Assign variables
+score1 = 94
+score2 = 91
+score3 = 85
+first_name = "Grace"
+last_name = "Hopper"
+class_curve = 2.0
+
+# Compute average score
+avg = (score1+score2+score3)/3
+
+# Display first and last name
+print('First semester grades for:', first_name, last_name)
+# Display individual scores
+print('Test scores: ', score1, ',', ' ', score2, ' and ', score3, sep = "")
+# Display average score
+print('Average Score:', avg)
+# Display average score with class curve
+print('Average Score + Class Curve:', avg + class_curve)
